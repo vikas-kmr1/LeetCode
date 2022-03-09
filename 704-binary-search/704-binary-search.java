50acda965d156cd495a08588359cd4ea76b4a6eb
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-03-09 19:14:14 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (86.79%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarySearch(nums,target,0,nums.length -1 );
+    }
+    
+    public int binarySearch(int[] nums,int target,int start,int end)
+    {
+        if (start<=end)
+        {
+            if (nums[start+(end-start)/2]==target)
+            {
+                return start+(end-start)/2;
+            }
+            else if (nums[start+(end-start)/2]<target)
+            {
+                return binarySearch(nums,target,(start+(end-start)/2)+1,end);
+            }
+            else{
+                return binarySearch(nums,target,0,(start+(end-start)/2)-1);
+            }
+            
+        }
+        else {return -1;}
+        
+    }
+}
