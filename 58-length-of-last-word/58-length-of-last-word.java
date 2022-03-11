8b246ca1040dd0850fcf657336e1c29e4a21749c
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-03-12 00:23:07 +0530

    Time: 0 ms (100.00%), Space: 42 MB (48.03%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.java
+++ 58-length-of-last-word/58-length-of-last-word.java
@@ -1,5 +1,7 @@
-class Solution:
-    def lengthOfLastWord(self, s: str) -> int:
-        lst=list(s.split())
-        return len(lst[-1])
-        
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        return s.length() - s.lastIndexOf(" ") -1; 
+
+    }
+}
