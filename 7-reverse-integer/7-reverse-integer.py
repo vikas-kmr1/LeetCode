6448bd3bc7919179234e16e02e4566b2b557aa9b
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-03-22 00:11:35 +0530

    Time: 47 ms (51.42%), Space: 13.8 MB (72.31%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -1,13 +1,21 @@
 def getRev(x,res):
-
-    if x>0:
+# using recustion
+#     if x>0:
+#         res = (res * 10) + (x%10)
+#         return getRev(x//10 , res )
+    
+#     if (res >= (2**31) - 1) or (res <= (2**31) * -1):
+#         return 0
+    
+#     else:return res
+    while x>0:
         res = (res * 10) + (x%10)
-        return getRev(x//10 , res )
-    
-    if (res >= (2**31) - 1) or (res <= (2**31) * -1):
-        return 0
-    
-    else:return res
+        if (res >= (2**31) - 1) or (res <= (2**31) * -1):
+            return 0
+        x= x//10  
+    return res
+
+
 
 
 class Solution:
