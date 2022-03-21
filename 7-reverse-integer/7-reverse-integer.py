ab22db67917b24bc2ededdb513f5aab86cba6a17
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-03-21 23:35:23 +0530

    Time: 52 ms (37.83%), Space: 14 MB (28.98%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.py
@@ -0,0 +1,28 @@
+def getRev(x,res):
+
+    if x>0:
+        res = (res * 10) + (x%10)
+        return getRev(x//10 , res )
+    
+    if (res >= (2**31) - 1) or (res <= (2**31) * -1):
+        return 0
+    
+    else:return res
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        if (x > (2**31) - 1) or (x < (2**31) * -1):
+            return 0
+        
+        else:    
+            signMultiplier = 1
+            if x < 0:
+                signMultiplier = -1
+                x = x * -1
+            
+            revNum = getRev(x,0)
+            
+            return signMultiplier * revNum
+        
