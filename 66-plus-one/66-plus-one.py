3a73389a2c79fe2e269fb555f6c2a2a0dfc7744c
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-03-23 18:22:03 +0530

    Time: 47 ms (50.07%), Space: 13.9 MB (67.29%) - LeetHub

--- 66-plus-one/66-plus-one.py
+++ 66-plus-one/66-plus-one.py
@@ -1,9 +1,35 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num = 0
-        for i in digits:
-            num = (num*10)  + i
-        num += 1
-        return list(map(int,str(num).strip()))
+
+    
+        for i in range(len(digits)-1,-1,-1):
+            if digits[i] != 9 :
+                digits[i] = digits[i] + 1
+                return digits
+
+            if digits[i] == 9:
+                digits[i] = 0
+                
+    
+        
+        if digits[0] == 0:
+            digits = [1] + digits
+        return digits
+            
+                
+                
+                
+    
+
+        
+        
+        # Brute-Force
+#         num = 0
+#         for i in digits:
+#             num = (num*10)  + i
+#         num += 1
+#         return list(map(int,str(num).strip()))
+
+
             
         
