11f56e0e252de6323bf716d3c163059789850261
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-05-22 14:43:00 +0530

    Time: 57 ms (22.83%), Space: 13.9 MB (69.70%) - LeetHub

---
+++ 28-implement-strstr/28-implement-strstr.py
@@ -0,0 +1,8 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle in haystack:
+            return haystack.find(needle)
+        return -1
+            
+        
+        
