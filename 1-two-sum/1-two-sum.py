6b3c0213930f915dcb26ee6ab84ccae1027f7071
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-06-19 13:11:29 +0530

    Time: 81 ms (73.04%), Space: 15.1 MB (64.66%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,13 +1,21 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        val_map = {}
+        for i in range(len(nums)) :
+            if (target - nums[i]) in val_map: 
+                return [val_map[target - nums[i]],i]
+            else :
+                val_map[nums[i]] = i
+        
     
-        for i in range(len(nums)-1):
-            l=target - nums[i]
-            try:
-                if l in nums:
-                    return [i,i+1+(nums[i+1:].index(l))]
-            except:
-                continue
+#         for i in range(len(nums)-1):
+#             l=target - nums[i]
+#             try:
+#                 if l in nums:
+#                     return [i,i+1+(nums[i+1:].index(l))]
+#             except:
+#                 continue
         
             
             
