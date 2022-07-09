e8283153db9f1d4976d5ece1d50276d554d480bc
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-07-09 23:31:25 +0530

    Time: 30 ms (96.87%), Space: 13.9 MB (44.45%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.py
+++ 392-is-subsequence/392-is-subsequence.py
@@ -4,7 +4,7 @@ class Solution:
         if len(s) == 0 :
             return True
         
-        if len(t) == 0:
+        if len(t) == 0 or len(s) > len(t):
             return False
         
         j = 0
@@ -12,7 +12,6 @@ class Solution:
             if j<len(s):
                 if i == s[j] :
                     j+=1
-              
         if j ==len(s):
             return True
         else:return False
