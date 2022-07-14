967635fdaae06fa49e43d9651002153e7c52bb3a
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-07-14 17:31:42 +0530

    Time: 808 ms (5.10%), Space: 27.8 MB (5.41%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,17 +1,19 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]):
 
-        return self.BinaryPeak(arr,0,len(arr)-1)
+        #return self.BinaryPeak(arr,0,len(arr)-1)
     
-    def BinaryPeak(self,ar, s,e):
+    # recursive approach
+    
+#     def BinaryPeak(self,ar, s,e):
         
-        if s>=e:
-            return s
+#         if s>=e:
+#             return s
         
-        elif ar[(s+e)//2] > ar[(s+e)//2 + 1]:
-            return self.BinaryPeak(ar,s,(s+e)//2)
-        else:
-            return self.BinaryPeak(ar,(s+e)//2+1,e)
+#         elif ar[(s+e)//2] > ar[(s+e)//2 + 1]:
+#             return self.BinaryPeak(ar,s,(s+e)//2)
+#         else:
+#             return self.BinaryPeak(ar,(s+e)//2+1,e)
         
     
     
@@ -19,16 +21,16 @@ class Solution:
         
         
         
-        # Iterative approach
-        # start=0
-        # end=len(arr)-1
-        # while start<end:
-        #     mid=int(start + (end-start)/2)
-        #     if arr[mid]>arr[mid+1]:
-        #         end=mid
-        #     else:
-        #         start=mid+1
-        # return start
+       # Iterative approach
+        start=0
+        end=len(arr)-1
+        while start<end:
+            mid=int(start + (end-start)/2)
+            if arr[mid]>arr[mid+1]:
+                end=mid
+            else:
+                start=mid+1
+        return start
     
     
         
