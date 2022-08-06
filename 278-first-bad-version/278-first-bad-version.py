f675745f5535d711095756b439bd63120267372b
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-06 18:45:32 +0530

    Time: 47 ms (48.74%), Space: 13.8 MB (61.83%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.py
+++ 278-first-bad-version/278-first-bad-version.py
@@ -3,18 +3,26 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        lt , rt = 1 , n
-        
-        while lt < rt:
-            pivot_version = lt + (rt - lt)//2
+            lt , rt = 1 , n
+            while lt < rt:
+                pivot_version = lt + (rt - lt)//2
             
-            if (isBadVersion(pivot_version)):
-                rt = pivot_version
-            
-            else:
-                lt = pivot_version + 1 
+                if (isBadVersion(pivot_version)):
+                    rt = pivot_version
+                else:
+                    lt = pivot_version + 1 
                 
-        return rt # can return lt as well both are same
+            return rt # can return lt as well both are same
+                
+    
+            
+        
                 
                 
         
+        
+        
+        
+        
+# iterative approach
+#        
