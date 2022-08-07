84cd6f43052a7323c342caa8c2445bd54a2c9464
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-07 16:16:44 +0530

    Time: 770 ms (29.72%), Space: 27.8 MB (13.21%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -1,39 +1,20 @@
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]):
-        return arr.index(max(arr))
 
-        #return self.BinaryPeak(arr,0,len(arr)-1)
-    
-# recursice approach    
-#     def BinaryPeak(self,ar, s,e):
-        
-#         if s>=e:
-#             return s
-        
-#         elif ar[(s+e)//2] > ar[(s+e)//2 + 1]:
-#             return self.BinaryPeak(ar,s,(s+e)//2)
-#         else:
-#             return self.BinaryPeak(ar,(s+e)//2+1,e)
-        
+        #Iterative approach
+        start=0
+        end=len(arr)-1
+        while start<end:
+            mid=int(start + (end-start)/2)
+            if arr[mid]>arr[mid+1]:
+                end=mid
+            else:
+                start=mid+1
+        return start
     
     
-        
-        
-        
-        
-        # Iterative approach
-        # start=0
-        # end=len(arr)-1
-        # while start<end:
-        #     mid=int(start + (end-start)/2)
-        #     if arr[mid]>arr[mid+1]:
-        #         end=mid
-        #     else:
-        #         start=mid+1
-        # return start
-    
-    
-        
+       # one -line
+       # return arr.index(max(arr))
         
         
         
