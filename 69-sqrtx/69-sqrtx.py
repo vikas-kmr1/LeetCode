2aa16a348117e0f7246c9ddef91cefeb7f6667cb
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-09 21:04:02 +0530

    Time: 46 ms (81.02%), Space: 13.8 MB (96.36%) - LeetHub

--- 69-sqrtx/69-sqrtx.py
+++ 69-sqrtx/69-sqrtx.py
@@ -1,9 +1,12 @@
+
+
+
 class Solution:
     def mySqrt(self, x: int) -> int:
         last_guess= x/2
         while True and last_guess>0:
             guess= (last_guess + x/last_guess)/2
-            if abs(guess - last_guess) < .000001: # example threshold
+            if abs(guess - last_guess) < .01: # example threshold
                 return int(guess)
             last_guess= guess
         return 0
