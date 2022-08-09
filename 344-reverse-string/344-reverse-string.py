76462d514c8d5d8cec77ff4e9778723742da1da4
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-09 19:02:08 +0530

    Time: 382 ms (20.94%), Space: 41.8 MB (8.94%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,4 +1,14 @@
+def reverseString(i:int ,s:List[str]):
+    if i >= len(s)//2:
+        return
+    else:
+        s[i],s[len(s)-i-1] = s[len(s)-i-1], s[i]
+        reverseString(i+1,s)
+        
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        s.reverse()
+        reverseString(0,s)
+        
+        
         
