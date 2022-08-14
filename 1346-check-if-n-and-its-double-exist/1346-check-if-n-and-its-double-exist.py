cdd359390d5bfb3e5ecfc1aeaba99d2fd49d8e3d
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-14 18:49:44 +0530

    Time: 106 ms (27.91%), Space: 13.9 MB (89.68%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,33 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        def binarySearch(nums,target)->bool:
+            low = 0 
+            high = len(nums) -1
+            
+            while low <= high:
+                mid = low + (high - low)//2
+                if nums[mid] == target:
+                    return True
+                elif nums[mid] > target:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            return False
+        
+        
+        arr.sort()
+        
+        for i in range(len(arr) - 1):
+            if arr[i] >= 0:
+                if binarySearch(arr[i+1:], arr[i]*2):
+                    return True
+            else:
+                if binarySearch(arr[:i], arr[i]*2):
+                    return True
+            
+        return False
+                
+            
+                
+        
+      
