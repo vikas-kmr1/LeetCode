ba7abee8ac2ee4ed12175971cae6071361c37659
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-14 18:25:37 +0530

    Time: 67 ms (57.41%), Space: 13.8 MB (57.67%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.py
+++ 441-arranging-coins/441-arranging-coins.py
@@ -1,15 +1,33 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        low,high = 1, n
-        ans  = 0
+        low,high = 0, n-1
         
-        while n > 0:
-            if n < low:
-                break
-            else:    
-                n -= low
-                low+=1
+        if n ==1:
+            return 1
+        
+        while low <= high:
+            mid = low + (high - low)//2
             
-        return low-1
+            if n == mid*(mid+1)//2:
+                return mid
+            elif n > mid*(mid+1)//2:
+                low  = mid + 1
+                ans = mid
+            else:
+                high = mid - 1
+                
+        return ans
+            
+        
+        
+        #brute force T.C = O(n)
+#         while n > 0:
+#             if n < low:
+#                 break
+#             else:    
+#                 n -= low
+#                 low+=1
+            
+#         return low-1
             
       
