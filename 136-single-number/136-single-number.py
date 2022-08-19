016d1eec7f5fcfd2a865151503a9a5cad3ba9157
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-19 16:03:24 +0530

    Time: 2564 ms (10.40%), Space: 16.9 MB (19.94%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,14 +1,25 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        while(len(nums)>0):
-            j = nums[0]
-            s = nums.count(j)
-            
-            if s == 1:
-                return j
+        
+        ans=[]
+        for i in nums:
+            if i not in ans:
+                ans.append(i)
             else:
+                ans.remove(i)
+                    
+        return ans[0]
+    
+    
+        
+#         while(len(nums)>0):
+#             j = nums[0]
+#             s = nums.count(j)
+#             if s == 1:
+#                 return j
+#             else:
                 
-                for i in range(s):
-                    nums.remove(j)
+#                 for i in range(s):
+#                     nums.remove(j)
             
         
