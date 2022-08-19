0358c567f4c1363d587229dc161a411dea3c7971
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-19 16:06:21 +0530

    Time: 242 ms (38.64%), Space: 16.6 MB (84.43%) - LeetHub

--- 136-single-number/136-single-number.py
+++ 136-single-number/136-single-number.py
@@ -1,6 +1,14 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        
         return reduce(lambda total, el: total ^ el, nums)
+        
+#         ans=[]
+#         for i in nums:
+#             if i not in ans:
+#                 ans.append(i)
+#             else:
+#                 ans.remove(i)
+                    
+#         return ans[0]
     
     
