fbd24e59eb54f65d6343320fc30621526242ca49
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-21 23:31:00 +0530

    Time: 187 ms (98.07%), Space: 0B (100.00%) - LeetHub

---
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+select max(Salary) as SecondHighestSalary 
+from Employee 
+where Salary not in (
+                     select max(Salary) 
+                        from Employee
+                    );
