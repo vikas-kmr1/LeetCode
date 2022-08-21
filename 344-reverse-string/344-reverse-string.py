01bd4e6fa14b400f837ef5b71a5b2f07bfd7cb23
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-21 14:16:29 +0530

    Time: 218 ms (85.86%), Space: 18.4 MB (86.20%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,14 +1,25 @@
-def reverseString(i:int ,s:List[str]):
-    if i >= len(s)//2:
-        return
-    else:
-        s[i],s[len(s)-i-1] = s[len(s)-i-1], s[i]
-        reverseString(i+1,s)
+# def reverseString(i:int ,s:List[str]):
+#     if i >= len(s)//2:
+#         return
+#     else:
+#         s[i],s[len(s)-i-1] = s[len(s)-i-1], s[i]
+#         reverseString(i+1,s)
         
 
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         reverseString(0,s)
+        
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        reverseString(0,s)
+        ptr1 = 0
+        ptr2 = len(s)-1
+        
+        while ptr1 < ptr2:
+            s[ptr1],s[ptr2]  = s[ptr2],s[ptr1]
+            ptr1+=1
+            ptr2-=1
+        
         
         
         
