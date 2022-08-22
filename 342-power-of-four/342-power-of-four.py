938633117191c07d5930b9c14bdcd086abe53853
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-22 17:09:33 +0530

    Time: 67 ms (14.93%), Space: 13.8 MB (95.34%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,5 +1,7 @@
+# class Solution:
+#     def isPowerOfFour(self, n: int) -> bool:
+#         return n > 0 and log(n, 4).is_integer()
+    
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        return n > 0 and log(n, 4).is_integer()
-    
-    
+        return n > 0 and not(n & (n - 1)) and n & 1431655765 == n
