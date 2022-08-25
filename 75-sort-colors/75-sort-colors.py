d5711f02611ebfd370ece879af1ec66f9589da60
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-25 19:06:03 +0530

    Time: 63 ms (24.72%), Space: 14 MB (15.69%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,7 +1,50 @@
+def partition(array, low, high):
+
+  # choose the last element as pivot
+  pivot = array[high]
+
+  # pointer for greater element
+  i = low - 1
+
+  # compare each element with pivot
+  for j in range(low, high):
+    if array[j] < pivot:
+      # if element smaller than pivot is found
+      # swap it with the greater element pointed by i
+      i = i + 1
+
+      # swapping element at i with element at j
+      (array[i], array[j]) = (array[j], array[i])
+
+  # swap the pivot element with the greater element specified by i
+  (array[i + 1], array[high]) = (array[high], array[i + 1])
+
+  # return the position from where partition is done
+  return i + 1
+
+# function to perform quicksort
+def quickSort(array, low, high):
+  if low < high:
+
+    pi = partition(array, low, high)
+
+    quickSort(array, low, pi - 1)
+
+    quickSort(array, pi + 1, high)
+
+
+
+
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        nums.sort()
+        quickSort(nums,0, len(nums) - 1)
+        
+                
+                
+            
+            
+        
         
