0c427957849f3f36857a7dd79f926b33d27f23ff
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-26 19:26:10 +0530

    Time: 474 ms (69.97%), Space: 0B (100.00%) - LeetHub

--- 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/586-customer-placing-the-largest-number-of-orders.sql
@@ -1,14 +1,26 @@
-select
+SELECT
     customer_number
-from
-    orders 
-group by 
+FROM
+    orders
+GROUP BY 
     customer_number
-having 
-    count(customer_number) >= all 
-    (select
-        count(customer_number) 
-     from 
-        orders 
-     group by 
-        customer_number);
+ORDER BY
+    COUNT(customer_number) DESC
+LIMIT 1;
+
+
+
+# select
+#     customer_number
+# from
+#     orders 
+# group by 
+#     customer_number
+# having 
+#     count(customer_number) >= all 
+#     (select
+#         count(customer_number) 
+#      from 
+#         orders 
+#      group by 
+#         customer_number);
