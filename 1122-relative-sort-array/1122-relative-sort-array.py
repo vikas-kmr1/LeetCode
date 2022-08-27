906b05d988a68abbdb153482fc6cfc5e0b0b79d1
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-27 14:59:10 +0530

    Time: 77 ms (24.25%), Space: 14 MB (39.50%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.py
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -1,3 +1,15 @@
+# class Solution:
+# #     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+# #         lst = []
+# #         for i in arr2:
+# #             lst.extend([i]*arr1.count(i))
+# #             for j in range(arr1.count(i)):
+# #                 arr1.remove(i)
+                
+# #         arr1.sort()
+# #         return lst+arr1
+            
+            
 class Solution:
     def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
         lst = []
@@ -5,8 +17,5 @@ class Solution:
             lst.extend([i]*arr1.count(i))
             for j in range(arr1.count(i)):
                 arr1.remove(i)
-                
         arr1.sort()
         return lst+arr1
-            
-            
