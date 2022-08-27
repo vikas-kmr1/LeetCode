859769495e22180228513126f6c29a653bad6e23
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-27 21:55:25 +0530

    Time: 124 ms (73.39%), Space: 23.2 MB (10.78%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from Queue import PriorityQueue
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        head = point = ListNode(0)
+        q = PriorityQueue()
+        for l in lists:
+            if l:
+                q.put((l.val, l))
+        
+    
+        while not q.empty():
+            val, node = q.get()
+            point.next = ListNode(val)
+            point = point.next
+            node = node.next
+            if node:
+                q.put((node.val, node))
+        return head.next
