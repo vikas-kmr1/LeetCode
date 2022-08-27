b51ec9ef486814bd74ca5c71c9862fc509e70651
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-27 19:52:16 +0530

    Time: 76 ms (67.09%), Space: 17.5 MB (67.21%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.py
+++ 141-linked-list-cycle/141-linked-list-cycle.py
@@ -4,14 +4,41 @@
 #         self.val = x
 #         self.next = None
 
+
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        current =  head
-        list_ = []
         
-        while(current):
-            if current in list_:
+        if not head:
+            return False
+        
+        slow = fast = head
+        
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+            
+            if slow == fast:
                 return True
-            list_.append(current)
-            current = current.next
+        return False
+            
+            
+        
+        
+        
+        
+        
+        
+        
+        
+# class Solution:
+#     def hasCycle(self, head: Optional[ListNode]) -> bool:
+#         current =  head
+#         list_ = []
+        
+#         while(current):
+#             if current in list_:
+#                 return True
+#             list_.append(current)
+#             current = current.next
         
