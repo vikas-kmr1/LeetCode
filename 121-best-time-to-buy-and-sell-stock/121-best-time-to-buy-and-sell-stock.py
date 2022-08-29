66380ce4b490cc428457d6cf6ecfea420f50889e
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-08-29 11:55:48 +0530

    Time: 1474 ms (62.41%), Space: 25.1 MB (38.33%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -4,19 +4,32 @@
     
 
 class Solution:
-    
+    # two pointer approach
     def maxProfit(self, prices: List[int]) -> int:
-        
-        max_pro =  0
-        mini = 1000001
-        
-        for i in range(len(prices)):
-            if(prices[i] < mini):
-                mini = prices[i]
+        max_pro = 0
+        l,r = 0,1
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                max_pro = max(prices[r] - prices[l],max_pro)
+            
             else:
-                max_pro = max(max_pro,prices[i] - mini )
-        
+                l = r 
+            r += 1 
         return max_pro
+        
+        
+        
+        # first approach
+#         max_pro =  0
+#         mini = 1000001
+        
+#         for i in range(len(prices)):
+#             if(prices[i] < mini):
+#                 mini = prices[i]
+#             else:
+#                 max_pro = max(max_pro,prices[i] - mini )
+        
+#         return max_pro
              
         
         
