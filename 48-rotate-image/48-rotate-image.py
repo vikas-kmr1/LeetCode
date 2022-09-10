6dd1c597269e7536286ce251de013f9721106a86
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-10 19:30:39 +0530

    Time: 73 ms (13.74%), Space: 14 MB (29.38%) - LeetHub

--- 48-rotate-image/48-rotate-image.py
+++ 48-rotate-image/48-rotate-image.py
@@ -1,10 +1,37 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         n = len(matrix[0])
-        for i in range(n // 2 + n % 2):
-            for j in range(n // 2):
-                tmp = matrix[n - 1 - j][i]
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
-                matrix[j][n - 1 - i] = matrix[i][j]
-                matrix[i][j] = tmp
+        
+        
+        #transpose matrix first:
+        
+        for i in range(n):
+            for j in range(i+1,n):
+                (matrix[i][j], matrix[j][i]) = (matrix[j][i], matrix[i][j])
+                
+            
+        # now reverese each column of the matrix:
+        
+        for col in matrix:
+                i = 0 
+                j = n-1
+                
+                while i < j:
+                    col[i],col[j] = col[j],col[i]
+                    i+= 1
+                    j-=1
+        
+                    
+                
+                
+        
+        
+        
+        
+        # for i in range(n // 2 + n % 2):
+        #     for j in range(n // 2):
+        #         tmp = matrix[n - 1 - j][i]
+        #         matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1]
+        #         matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i]
+        #         matrix[j][n - 1 - i] = matrix[i][j]
+        #         matrix[i][j] = tmp
