a10cd23a0925e58852eba0babf5dd7e145d167af
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-10 20:41:34 +0530

    LC *88 updated

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -1,8 +1,28 @@
 class Solution:
-
-    
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        for i in range(n):
-            nums1[m+i]= nums2[i]
-        nums1.sort()
+    
+        last = m+n - 1
+
+        while n and m:
+            
+            if nums1[m-1] < nums2[n-1]:
+                nums1[last] = nums2[n-1]
+                n -= 1
+                
+                
+            else:
+                nums1[last] = nums1[m-1]
+                m -= 1
+                
+            last -= 1
+            
+        while n:
+            nums1[last] = nums2[n-1]
+            n = n-1
+            last -= 1
+            
+                
+            
         
+        
+    
