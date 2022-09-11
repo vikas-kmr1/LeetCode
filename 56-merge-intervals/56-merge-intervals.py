ac77882a3774a4258597e83319569f905925f708
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-12 00:36:40 +0530

    Time: 295 ms (19.90%), Space: 18 MB (98.70%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,30 +1,48 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+
+        intervals.sort(key=lambda x: x[0])
+
+        merged = []
+        for interval in intervals:
+            # if the list of merged intervals is empty or if the current
+            # interval does not overlap with the previous, simply append it.
+            if not merged or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+            # otherwise, there is overlap, so we merge the current and previous
+            # intervals.
+                merged[-1][1] = max(merged[-1][1], interval[1])
+
+        return merged
+
+#class Solution:
+#     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         
-        if len(intervals) <= 1:
-            return intervals
+#         if len(intervals) <= 1:
+#             return intervals
         
     
-        result  = []
+#         result  = []
         
-        intervals.sort()
+#         intervals.sort()
         
-        minX =intervals[0][0]
-        maxY =intervals[0][1]
+#         minX =intervals[0][0]
+#         maxY =intervals[0][1]
         
-        for i in  range(1,len(intervals) ):
-            a,b = intervals[i][0],intervals[i][1]
+#         for i in  range(1,len(intervals) ):
+#             a,b = intervals[i][0],intervals[i][1]
         
-            if a <= maxY:
-                maxY = max([maxY,a,b])
+#             if a <= maxY:
+#                 maxY = max([maxY,a,b])
             
-            else:
-                result.append([minX,maxY])
-                minX = a
-                maxY = b
+#             else:
+#                 result.append([minX,maxY])
+#                 minX = a
+#                 maxY = b
         
-        result.append([minX,maxY])
-        return result
+#         result.append([minX,maxY])
+#         return result
             
             
                 
