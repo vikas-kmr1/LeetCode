2c20ca8191c238cee3f2e1d9ea130c58f47520b9
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-12 00:34:37 +0530

    Time: 408 ms (5.09%), Space: 18.9 MB (26.05%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.py
@@ -0,0 +1,31 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
+        if len(intervals) <= 1:
+            return intervals
+        
+    
+        result  = []
+        
+        intervals.sort()
+        
+        minX =intervals[0][0]
+        maxY =intervals[0][1]
+        
+        for i in  range(1,len(intervals) ):
+            a,b = intervals[i][0],intervals[i][1]
+        
+            if a <= maxY:
+                maxY = max([maxY,a,b])
+            
+            else:
+                result.append([minX,maxY])
+                minX = a
+                maxY = b
+        
+        result.append([minX,maxY])
+        return result
+            
+            
+                
+          
