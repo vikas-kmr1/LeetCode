41348df3520d5a950314cf6b7602de0a048e3723
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-12 00:52:57 +0530

    Time: 351 ms (6.73%), Space: 18.8 MB (26.05%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -17,7 +17,7 @@ class Solution:
             # a,b = intervals[i][0],intervals[i][1]
         
             if a <= maxY:
-                maxY = max([maxY,a,b])
+                maxY = max([maxY,b])
                 
             else:
                 result.append([minX,maxY])
