2306b6d543920bbae6d0d1ec52bc109b38fc6283
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-12 01:01:55 +0530

    Time: 295 ms (19.90%), Space: 18.9 MB (26.05%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,28 +1,43 @@
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        
-        if len(intervals) <= 1:
-            return intervals
-            
         intervals.sort()
-        result  = []
-        minX =intervals[0][0]
-        maxY =intervals[0][1]
+        result  = [intervals[0]]
         
         for a,b in intervals[1:]:
-            # a,b = intervals[i][0],intervals[i][1]
         
-            if a <= maxY:
-                maxY = max([maxY,b])
+           
+            if a <= result[-1][1]:
+                result[-1][1] = max(result[-1][1] ,b)
                 
             else:
-                result.append([minX,maxY])
-                minX = a
-                maxY = b
-        
-        result.append([minX,maxY])
+                result.append([a,b])
+                
         return result
+            
+            
+        
+#         if len(intervals) <= 1:
+#             return intervals
+            
+#         intervals.sort()
+#         result  = []
+#         minX =intervals[0][0]
+#         maxY =intervals[0][1]
+        
+#         for a,b in intervals[1:]:
+#             # a,b = intervals[i][0],intervals[i][1]
+        
+#             if a <= maxY:
+#                 maxY = max([maxY,b])
+                
+#             else:
+#                 result.append([minX,maxY])
+#                 minX = a
+#                 maxY = b
+        
+#         result.append([minX,maxY])
+#         return result
             
             
                 
