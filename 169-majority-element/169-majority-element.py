52c41cf14f80bbb2bf78cda2978d9ebabcab16fb
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-13 00:25:09 +0530

    Time: 354 ms (14.91%), Space: 15.4 MB (86.02%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,14 +1,18 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         size = len(nums)
-        while(len(nums)):
-            n = nums[0]
-            s  = nums.count(n)
+        hash_map = Counter(nums)
+        
+        return max(hash_map.keys(), key = hash_map.get )
+        
+#         while(len(nums)):
+#             n = nums[0]
+#             s  = nums.count(n)
             
-            if s >= len(nums)/2:
-                return n
-            else:
-                for i in range(s):
-                    nums.remove(n)
+#             if s >= len(nums)/2:
+#                 return n
+#             else:
+#                 for i in range(s):
+#                     nums.remove(n)
                     
         
