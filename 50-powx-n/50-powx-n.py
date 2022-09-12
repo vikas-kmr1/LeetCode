b1771c535711bdfb753ac4769e582974704dbeb5
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-12 23:51:32 +0530

    Time: 69 ms (6.99%), Space: 13.8 MB (67.94%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,15 +1,15 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        
-        def helper(x:int,n:int):
-            if x == 0: return 0
-            if n == 0: return 1
+        return x**n
+#         def helper(x:int,n:int):
+#             if x == 0: return 0
+#             if n == 0: return 1
             
-            res = helper(x,n//2)
-            res = res * res
+#             res = helper(x,n//2)
+#             res = res * res
             
-            return x*res if n%2 else res
+#             return x*res if n%2 else res
         
-        ans =  helper(x,abs(n)) 
+#         ans =  helper(x,abs(n)) 
         
-        return ans if n>=0 else 1/ans        
+#         return ans if n>=0 else 1/ans        
