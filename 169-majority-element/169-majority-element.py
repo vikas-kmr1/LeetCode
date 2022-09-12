fc76fa53f856f8e3a6321bd5505c41db134a552f
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-13 00:30:04 +0530

    Time: 362 ms (13.22%), Space: 15.5 MB (86.02%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,18 +1,18 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        size = len(nums)
-        hash_map = Counter(nums)
+    def majorityElement(self, nums):
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
+
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         size = len(nums)
+#         hash_map = Counter(nums)
         
-        return max(hash_map.keys(), key = hash_map.get )
-        
-#         while(len(nums)):
-#             n = nums[0]
-#             s  = nums.count(n)
-            
-#             if s >= len(nums)/2:
-#                 return n
-#             else:
-#                 for i in range(s):
-#                     nums.remove(n)
-                    
-        
+#         return max(hash_map.keys(), key = hash_map.get )
