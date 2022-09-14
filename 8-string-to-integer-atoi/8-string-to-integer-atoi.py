dc51710fbf3cb818b1cbf1bed09fddcf620f7126
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-14 15:48:46 +0530

    Time: 82 ms (5.29%), Space: 14 MB (28.65%) - LeetHub

---
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -0,0 +1,33 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        sign = 0
+        result = 0
+        trimmed = 0
+        
+        for ch in s:
+            if ch == ' ' and trimmed == 0:
+                s = s[1:]
+            else:
+                trimmed = 1
+         
+        if s == "":
+            return result
+        
+        if s[0] in ['-','+']:
+            if s[0] == '-':
+                sign = -1
+            else:
+                sign = +1
+            s = s[1:]
+                
+        if (sign == 0):
+            sign = 1
+        
+        for ch in s:
+            if ch.isdigit():
+                result = result*10 + int(ch)
+            else:
+                break
+        
+        return max(-2**31, min(sign * result, 2**31-1))
+        
