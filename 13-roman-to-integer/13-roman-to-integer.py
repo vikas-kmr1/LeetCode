5fd21c38d126028128afaaa2ab517df1a27dae5a
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-14 14:23:23 +0530

    Time: 86 ms (18.63%), Space: 13.3 MB (81.62%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        dic={"I":1,
+        roman={"I":1,
              "V":5,
              "X":10,
              "L":50,
@@ -17,19 +17,24 @@ class Solution(object):
              "XC":90,
              "CD":400,
              "CM":900
-            
         }
+        
         num=0
-        i=0
-        while(i<len(s)):
+        lst = 0
+        for i in s[::-1]:
+            n = roman.get(i)
+            if  n >= lst:
+                num += n
             
-            if ((i<(len(s)-1))) and( s[i:i+2]=='IV' or  s[i:i+2]=='IX' or s[i:i+2]=="XL" or s[i:i+2]=="XC" or s[i:i+2]=="CD" or s[i:i+2]=="CM"):
-                num+=dic.get(s[i:i+2])
-                print(dic.get(s[i:i+2]))  
-                i+=1
             else:
-                num+=dic.get(s[i])
-                print(dic.get(s[i]))
-            i+=1
+                num -= n
+            lst = n
+            
+        return num
+            
+            
+            
+            
+
         return num    
             
