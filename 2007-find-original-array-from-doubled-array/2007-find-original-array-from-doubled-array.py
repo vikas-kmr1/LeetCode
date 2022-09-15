af22c529b05eaeb980a2b102c34931fe41129cc4
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-15 11:09:32 +0530

    Time: 3271 ms (13.62%), Space: 32 MB (75.26%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.py
@@ -1,24 +1,37 @@
+# class Solution:
+#     def findOriginalArray(self, changed: List[int]) -> List[int]:
+        
+#         if len(changed)%2 == 1:
+#             return []
+        
+#         changed.sort()
+#         original = []
+#         hashmap  = Counter(changed)
+        
+#         for n in changed:
+#             if not hashmap.get(n):
+#                 continue
+            
+#             if not hashmap.get(n*2) :
+#                 return []
+            
+#             original.append(n)
+#             hashmap[n]-=1
+#             hashmap[n*2] -= 1
+        
+#         return original
+            
 class Solution:
     def findOriginalArray(self, changed: List[int]) -> List[int]:
-        
-        if len(changed)%2 == 1:
-            return []
-        
-        changed.sort()
-        original = []
-        hashmap  = Counter(changed)
-        
-        for n in changed:
-            if not hashmap.get(n):
-                continue
-            
-            if not hashmap.get(n*2) :
-                return []
-            
-            original.append(n)
-            hashmap[n]-=1
-            hashmap[n*2] -= 1
-        
-        return original
-            
-  
+        c = Counter(changed)
+
+        zeros, m = divmod(c[0], 2)
+        if m: return []
+        ans = [0]*zeros   
+
+        for n in sorted(c.keys()):
+            if c[n] > c[2*n]: return []
+            c[2*n]-= c[n]
+            ans.extend([n]*c[n])
+
+        return ans
