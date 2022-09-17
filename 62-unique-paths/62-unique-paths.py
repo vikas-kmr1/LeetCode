1124000a30204076e351917e2c458a245b62cd40
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-17 12:07:54 +0530

    Time: 60 ms (24.16%), Space: 13.9 MB (74.80%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,14 +1,21 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def gridTravellor(m,n,memo={}):
-            key = m,n
-            if memo.get(key):
-                return memo[key]
-            if m==1 and n==1:
-                return 1
-            if m==0 or n==0:
-                return 0
-            else:
-                memo[key] = gridTravellor(m-1, n, memo) + gridTravellor(m, n-1, memo)
-            return memo[key]
-        return gridTravellor(m,n)
+        row = [1] * n
+        for i in range( m - 1 ):
+            newRow = [1]*n
+            for j in range(n - 2 , -1 , -1 ) :
+                newRow[ j ] = newRow[ j + 1 ] + row[ j ]
+            row = newRow
+        return row[0]
+        # def gridTravellor(m,n,memo={}):
+        #     key = m,n
+        #     if memo.get(key):
+        #         return memo[key]
+        #     if m==1 and n==1:
+        #         return 1
+        #     if m==0 or n==0:
+        #         return 0
+        #     else:
+        #         memo[key] = gridTravellor(m-1, n, memo) + gridTravellor(m, n-1, memo)
+        #     return memo[key]
+        # return gridTravellor(m,n)
