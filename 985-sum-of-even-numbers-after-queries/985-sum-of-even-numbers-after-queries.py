3e18d3d6f63f8899fb0f143880f243fffe898d08
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-21 12:46:33 +0530

    Time: 539 ms (91.58%), Space: 20.5 MB (43.88%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.py
@@ -1,6 +1,7 @@
 class Solution:
     def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
-        
+        #T.C : O(N+Q), where N is the length of A and Q is the number of queries
+        #S.C : O(Q)O(Q), though we only allocate O(1)O(1) additional space.
         even_sum = sum(n for n in nums if n%2 == 0)
         ans = []
         
