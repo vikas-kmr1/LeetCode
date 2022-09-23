91db12e842352b196f1eb8ca41aa514bd3c3d634
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-23 10:54:56 +0530

    Time: 3105 ms (38.58%), Space: 13.8 MB (97.64%) - LeetHub

---
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -0,0 +1,21 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        M = 10 ** 9 + 7
+        # `l` is the bit length to be shifted
+        l, ans = 0, 0
+        for i in range(1, n + 1):
+            # i & (i - 1) means removing the rightmost set bit
+            # e.g. 100100 -> 100000
+            #      000001 -> 000000
+            #      000000 -> 000000
+            # after removal, if it is 0, then it means it is power of 2
+            # as all power of 2 only contains 1 set bit
+            # if it is power of 2, we increase the bit length `l`
+            if i & (i - 1) == 0:
+                l += 1
+            # (ans << l) means shifting the orginal answer `l` bits to th left
+            # (x | i) means  using OR operation to set the bit
+            # e.g. 0001 << 3 = 0001000
+            # e.g. 0001000 | 0001111 = 0001111
+            ans = ((ans << l) | i) % M
+        return ans
