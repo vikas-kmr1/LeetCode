ca695b9d74a0c2b5dcc53eccc079c6c2b767c7ee
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-23 18:14:05 +0530

    Time: 147 ms (20.24%), Space: 13.9 MB (43.21%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -3,17 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+# Time complexity : O(\max(m, n))O(max(m,n)). Assume that mm and nn represents the length of l1 and l2 respectively, the algorithm above iterates at most \max(m, n)max(m,n) times.
+
+# Space complexity : O(\max(m, n))O(max(m,n)). The length of the new list is at most \max(m,n) + 1max(m,n)+1
+
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         
         head = ListNode(0) # creating new listed list that stores result
         current = head
         
-        carry = 0
+        carry = 0 # To store carry of sum  
         
         while l1 or l2 or carry != 0:
-            a = l1.val if l1 else 0
-            b = l2.val if l2 else 0
+            
+            a = l1.val if l1 else 0  #if l1 not exist a=0
+            b = l2.val if l2 else 0  #if l1 not exist b=0
         
             two_sum = (a+b+carry)
             carry = two_sum//10
@@ -23,7 +30,6 @@ class Solution:
             l1 = l1.next if l1 else None
             l2 = l2.next if l2 else None
     
-        
         return head.next
     
             
