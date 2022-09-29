3761f7286cb85f117320ccd0fb0d9dcb7df79d41
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-29 22:17:49 +0530

    Time: 495 ms (31.69%), Space: 21.3 MB (82.00%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -15,12 +15,21 @@
 
 # Time Complexity : O(N)
 # Space Complexity : O(1)
+# class Solution:
+#     def productExceptSelf(self, nums):
+#         ans ,suf, pre = [1]*len(nums),1,1
+#         for i in range(len(nums)):
+#             ans[i] *= pre
+#             pre *= nums[i]  # prefix product from one end
+#             ans[-1-i] *= suf
+#             suf *= nums[-1-i]   # suffix product from other end
+#         return ans
 class Solution:
     def productExceptSelf(self, nums):
-        ans ,suf, pre = [1]*len(nums),1,1
-        for i in range(len(nums)):
-            ans[i] *= pre
-            pre *= nums[i]  # prefix product from one end
-            ans[-1-i] *= suf
-            suf *= nums[-1-i]   # suffix product from other end
+        n, ans, suffix_prod = len(nums), [1]*len(nums), 1
+        for i in range(1,n):
+            ans[i] = ans[i-1] * nums[i-1]
+        for i in range(n-1,-1,-1):
+            ans[i] *= suffix_prod
+            suffix_prod *= nums[i]
         return ans
