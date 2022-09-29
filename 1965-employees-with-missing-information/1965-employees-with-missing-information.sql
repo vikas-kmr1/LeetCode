3e7fa98b34515f43f50255ba24001866916e00a0
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-29 10:56:29 +0530

    Time: 501 ms (82.06%), Space: 0B (100.00%) - LeetHub

--- 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -1,11 +1,35 @@
 # Write your MySQL query statement below
-select  employee_id
-from Employees 
-where employee_id not in(select employee_id
-                        from Salaries)
-             union
-select  employee_id
-from  Salaries
-where employee_id not in(select employee_id
-                        from Employees)
-order by employee_id;
+
+SELECT 
+    E.EMPLOYEE_ID
+FROM 
+    EMPLOYEES E
+WHERE 
+    E.EMPLOYEE_ID NOT IN
+                        (SELECT S.EMPLOYEE_ID FROM SALARIES S)  
+            
+            UNION
+            
+SELECT 
+    S.EMPLOYEE_ID
+FROM 
+    SALARIES S 
+WHERE 
+    S.EMPLOYEE_ID NOT IN
+                        (SELECT E.EMPLOYEE_ID FROM EMPLOYEES E)  
+
+ORDER BY
+    EMPLOYEE_ID ASC
+            
+
+
+# select  employee_id
+# from Employees 
+# where employee_id not in(select employee_id
+#                         from Salaries)
+#              union
+# select  employee_id
+# from  Salaries
+# where employee_id not in(select employee_id
+#                         from Employees)
+# order by employee_id;
