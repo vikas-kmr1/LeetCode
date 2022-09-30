e1fe469117fb5df5117f2e9f0df0ecb8dbb01cdf
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-30 20:49:18 +0530

    Time: 1813 ms (46.46%), Space: 18.2 MB (20.89%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -4,6 +4,8 @@ class Solution:
         res = []
         nums.sort() # Takes O(nlogn ) ans space: o(n)
         
+        
+        # Takes O(n*N) 
         for i,n in enumerate(nums):
             
             if i > 0 and n == nums[i-1]:
@@ -26,7 +28,9 @@ class Solution:
                     while nums[l] == nums[l-1] and l < r:
                         l += 1
         return res
-                        
+        
+        #T.C: O(nlogN) + O(N*N) = O(N*N)
+        #S.C: O(N) sort takes n extra space
                 
                 
         
