8616a2c7da6a441a74d77a0f8d98204cdea6e6a5
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-09-30 23:15:59 +0530

    Time: 62 ms (20.88%), Space: 13.8 MB (82.96%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.py
+++ 54-spiral-matrix/54-spiral-matrix.py
@@ -3,30 +3,30 @@ class Solution:
         # T.C: O(m*n) where m is no. of rows and n is the no. of columns 
         # S.C: O(m*n) if does include the output space else O(1)
         res = []
-        left, right = 0 , len(matrix[0])
-        top, bottom = 0 , len(matrix)
+        left, right = 0 , len(matrix[0]) - 1
+        top, bottom = 0 , len(matrix) -1
         
-        while left < right and top < bottom:
+        while left <= right and top <= bottom:
             #get every ith element in top row
-            for i in range(left,right):
+            for i in range(left,right+1):
                 res.append(matrix[top][i])
             top += 1
             
             #get every i element in right column
-            for i in range(top,bottom):
-                res.append(matrix[i][right-1])
+            for i in range(top,bottom+1):
+                res.append(matrix[i][right])
             right -= 1
             
-            if not (left < right and top < bottom):
+            if not (left <= right and top <= bottom):
                 break
             
             #get every ith element in bottom row:
-            for i in range(right-1,left-1,-1):
-                res.append(matrix[bottom-1][i])
+            for i in range(right,left-1,-1):
+                res.append(matrix[bottom][i])
             bottom -= 1
             
             # get every ith element in left column:
-            for i in range(bottom-1,top-1,-1):
+            for i in range(bottom,top-1,-1):
                 res.append(matrix[i][left])
             left += 1
             
