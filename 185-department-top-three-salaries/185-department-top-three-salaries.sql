62949788bebd6857c8c0fa80706c56f5222519d5
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-10-01 12:52:42 +0530

    Time: 851 ms (72.41%), Space: 0B (100.00%) - LeetHub

--- 185-department-top-three-salaries/185-department-top-three-salaries.sql
+++ 185-department-top-three-salaries/185-department-top-three-salaries.sql
@@ -1,16 +1,25 @@
-SELECT
-    d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
-FROM
-    Employee e1
-        JOIN
-    Department d ON e1.DepartmentId = d.Id
-WHERE
-    3 > (SELECT
-            COUNT(DISTINCT e2.Salary)
-        FROM
-            Employee e2
-        WHERE
-            e2.Salary > e1.Salary
-                AND e1.DepartmentId = e2.DepartmentId
-        )
-;
+# SELECT
+#     d.Name AS 'Department', e1.Name AS 'Employee', e1.Salary
+# FROM
+#     Employee e1
+#         JOIN
+#     Department d ON e1.DepartmentId = d.Id
+# WHERE
+#     3 > (SELECT
+#             COUNT(DISTINCT e2.Salary)
+#         FROM
+#             Employee e2
+#         WHERE
+#             e2.Salary > e1.Salary
+#                 AND e1.DepartmentId = e2.DepartmentId
+#         )
+# ;
+
+select d.Name as Department, a. Name as Employee, a. Salary 
+from (
+select e.*, dense_rank() over (partition by DepartmentId order by Salary desc) as DeptPayRank 
+from Employee e 
+) a 
+join Department d
+on a. DepartmentId = d. Id 
+where DeptPayRank <=3;
