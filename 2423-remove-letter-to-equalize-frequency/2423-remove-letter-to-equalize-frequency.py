9ceacae11548be6f3a8d6085e3ae4834ee2a04f6
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-10-02 00:03:08 +0530

    Time: 63 ms (100.00%), Space: 13.7 MB (100.00%) - LeetHub

---
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+class Solution:
+    def equalFrequency(self, word: str) -> bool:
+        
+        # [1] count letter frequencies
+        freq = Counter(word)
+        
+        # [2] check several frequency conditions
+        
+        #     - for a single unique letter in the word
+        if  len(freq.most_common()) == 1:
+            return True
+
+        #     - for all frequencies being equal to 1
+        if  freq.most_common()[0][1] == 1:
+            return True
+
+        #     - for one most frequent element
+        if  freq.most_common()[0][1] - freq.most_common()[1][1] == 1 \
+        and freq.most_common()[1][1] == freq.most_common()[-1][1]:
+                return True
+
+        #     - for one less frequent element
+        if  freq.most_common()[-2][1] - freq.most_common()[-1][1] == 1 \
+        and freq.most_common()[0][1] == freq.most_common()[-2][1]:
+                return True
+                
+        return False
