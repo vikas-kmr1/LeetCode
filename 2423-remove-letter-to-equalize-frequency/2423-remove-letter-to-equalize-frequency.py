efdcd681a3f56dd866034735bdf9f74d2f6a0978
Vikas Kumar <73611313+vickyrules@users.noreply.github.com>
2022-10-02 00:09:34 +0530

    Time: 67 ms (100.00%), Space: 14 MB (100.00%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.py
@@ -17,13 +17,11 @@ class Solution:
             return True
 
         #     - for one most frequent element
-        if  freq.most_common()[0][1] - freq.most_common()[1][1] == 1 \
-        and freq.most_common()[1][1] == freq.most_common()[-1][1]:
+        if  freq.most_common()[0][1] - freq.most_common()[1][1] == 1 and freq.most_common()[1][1] == freq.most_common()[-1][1]:
                 return True
 
         #     - for one less frequent element
-        if  freq.most_common()[-2][1] - freq.most_common()[-1][1] == 1 \
-        and freq.most_common()[0][1] == freq.most_common()[-2][1]:
+        if  freq.most_common()[-2][1] - freq.most_common()[-1][1] == 1 and freq.most_common()[0][1] == freq.most_common()[-2][1]:
                 return True
                 
         return False
