cbd56843a249f126115c26fcd4024cf631c5527b
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2022-10-18 21:16:05 +0530

    Time: 466 ms (33.66%), Space: 17.2 MB (74.27%) - LeetHub

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,69 +1,31 @@
-class Solution:
-    
-    def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
-        
-        # Check for an empty matrix
-        if not matrix or not matrix[0]:
-            return []
-        
-        # The dimensions of the matrix
-        N, M = len(matrix), len(matrix[0])
-        
-        # Incides that will help us progress through 
-        # the matrix, one element at a time.
-        row, column = 0, 0
-        
-        # As explained in the article, this is the variable
-        # that helps us keep track of what direction we are
-        # processing the current diaonal
-        direction = 1
-        
-        # Final result array that will contain all the elements
-        # of the matrix
-        result = []
-        
-        # The uber while loop which will help us iterate over all
-        # the elements in the array.
-        while row < N and column < M:
-            
-            # First and foremost, add the current element to 
-            # the result matrix. 
-            result.append(matrix[row][column])
-            
-            # Move along in the current diagonal depending upon
-            # the current direction.[i, j] -> [i - 1, j + 1] if 
-            # going up and [i, j] -> [i + 1][j - 1] if going down.
-            new_row = row + (-1 if direction == 1 else 1)
-            new_column = column + (1 if direction == 1 else -1)
-            
-            # Checking if the next element in the diagonal is within the
-            # bounds of the matrix or not. If it's not within the bounds,
-            # we have to find the next head. 
-            if new_row < 0 or new_row == N or new_column < 0 or new_column == M:
-                
-                # If the current diagonal was going in the upwards
-                # direction.
-                if direction:
-                    
-                    # For an upwards going diagonal having [i, j] as its tail
-                    # If [i, j + 1] is within bounds, then it becomes
-                    # the next head. Otherwise, the element directly below
-                    # i.e. the element [i + 1, j] becomes the next head
-                    row += (column == M - 1)
-                    column += (column < M - 1)
+class Solution(object):
+    def findDiagonalOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        d={}
+		#loop through matrix
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+				#if no entry in dictionary for sum of indices aka the diagonal, create one
+                if i + j not in d:
+                    d[i+j] = [matrix[i][j]]
                 else:
+				#If you've already passed over this diagonal, keep adding elements to it!
+                    d[i+j].append(matrix[i][j])
                     
-                    # For a downwards going diagonal having [i, j] as its tail
-                    # if [i + 1, j] is within bounds, then it becomes
-                    # the next head. Otherwise, the element directly below
-                    # i.e. the element [i, j + 1] becomes the next head
-                    column += (row == N - 1)
-                    row += (row < N - 1)
-                    
-                # Flip the direction
-                direction = 1 - direction        
+        #print(d)
+		# we're done with the pass, let's build our answer array
+        ans= []
+		#look at the diagonal and each diagonal's elements
+        for entry in d.items():
+			#each entry looks like (diagonal level (sum of indices), [elem1, elem2, elem3, ...])
+			#snake time, look at the diagonal level
+            if entry[0] % 2 == 0:
+				#Here we append in reverse order because its an even numbered level/diagonal. 
+                ans.extend(entry[1][::-1])
             else:
-                row = new_row
-                column = new_column
-                        
-        return result                 
+                ans.extend(entry[1])
+        return ans
+                
