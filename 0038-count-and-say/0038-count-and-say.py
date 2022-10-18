416fc19ef305177abd8da153693ada628385bf31
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2022-10-18 13:26:34 +0530

    Time: 96 ms (38.36%), Space: 14.3 MB (7.99%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        s = '1'
+        for _ in range(n - 1):
+            s = ''.join(str(len(group)) + digit
+                for group, digit in re.findall(r'((.)\2*)', s))
+        return s
+        
