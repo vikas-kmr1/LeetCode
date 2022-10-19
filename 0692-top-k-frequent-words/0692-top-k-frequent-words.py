d4a849e32b2db736cf41b947c8872c824038e35b
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2022-10-19 19:48:05 +0530

    Time: 110 ms (54.84%), Space: 14.1 MB (27.71%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,42 @@
+import collections
+import heapq
+import functools
+
+@functools.total_ordering
+class Element:
+    def __init__(self, count, word):
+        self.count = count
+        self.word = word
+        
+    def __lt__(self, other):
+        if self.count == other.count:
+            return self.word > other.word
+        return self.count < other.count
+    
+    def __eq__(self, other):
+        return self.count == other.count and self.word == other.word
+
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        counts = collections.Counter(words)   
+        
+        freqs = []
+        heapq.heapify(freqs)
+        for word, count in counts.items():
+            heapq.heappush(freqs, (Element(count, word), word))
+            if len(freqs) > k:
+                heapq.heappop(freqs)
+        
+        res = []
+        for _ in range(k):
+            res.append(heapq.heappop(freqs)[1])
+        return res[::-1]
+            
+                
+        
+        
