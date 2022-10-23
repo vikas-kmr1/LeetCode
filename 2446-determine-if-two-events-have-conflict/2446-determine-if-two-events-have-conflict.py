6fc1c863c0f26e8f82600294a23a52f57aaf1325
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2022-10-23 09:48:20 +0530

    Time: 65 ms (33.33%), Space: 13.8 MB (33.33%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
@@ -1,7 +1,7 @@
 
 class Solution:
     def haveConflict(self, e1: List[str], e2: List[str]) -> bool:
-        return max(e1[0],e2[0]) <= min(e1[1],e2[1])
+          return e1[0] <= e2[1] and e2[0] <= e1[1]
         
         
         
