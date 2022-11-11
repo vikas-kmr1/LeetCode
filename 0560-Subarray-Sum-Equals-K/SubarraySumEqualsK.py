75c32e05c3bbd817db9e0a94c1bd3bee9d41f64b
ᯓνιкαѕ кυмαяᯓ <VICKYRULES1705@GMAIL.COM>
2022-11-11 21:05:20 +0530

    Runtime: 884 ms Beats: 14.18%, Memory:16.5 MB Beats: 75.11%

---
+++ 0560-Subarray-Sum-Equals-K/SubarraySumEqualsK.py
@@ -0,0 +1,17 @@
+## Prefix sum approach
+#T.C: O(n)
+#S.C: O(n)
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        sums = 0
+        d = {0:1}
+        
+        for i in range(len(nums)):
+            sums += nums[i]
+            count += d.get(sums-k,0)
+            d[sums] = d.get(sums,0) + 1
+        
+        return(count)
+        
