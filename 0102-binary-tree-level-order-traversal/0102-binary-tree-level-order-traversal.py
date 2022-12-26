1a7d15c611ccd01d6194129ff4cc1e31bb4b5afb
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2022-12-26 19:00:49 +0530

    Time: 33 ms (95.29%), Space: 14.9 MB (17.90%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -0,0 +1,69 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        res = []
+        if not root: return []
+
+        def helper(root,ind):
+            nonlocal res
+            if  len(res) < ind+1 :
+                res.append([root.val])
+            elif len(res) >= ind+1:
+                res[ind].append(root.val)
+            
+            if root.left: helper(root.left,ind+1)
+            if root.right: helper(root.right,ind+1)
+
+        helper(root,0)
+        return res
+
+# class Solution:
+#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+#         # if root not exist return 
+#         if not root:
+#             return []
+        
+        
+#         #using BFS
+        
+#         ans = [] #(list of lists storing ) level order lists 
+#         queue  = collections.deque()
+#         queue.append(root) # pushing root or source node in the queue
+    
+    
+#         # run untill queue is not empty
+#         while queue:        
+#             queue_size = len(queue)
+            
+#             lst = []
+#             # run for each node in the queue
+            
+#             for _ in range(queue_size):
+#                 node = queue.popleft()
+                
+#                 lst.append(node.val)
+                
+#                 if node.left:
+#                     queue.append(node.left)
+                
+#                 if node.right:
+#                     queue.append(node.right)
+
+#             #adding the level ordered list
+#             if lst:
+#                 ans.append(lst)
+#         return ans
+            
+        
+                
+            
+            
+                
+        
