24c8af9eb938bea2266e86af288ccd9b4aa8bfb2
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2022-12-27 18:06:58 +0530

    Time: 93 ms (84.75%), Space: 20.8 MB (29.36%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,11 +1,22 @@
+# # T.C: O( n.log(n) )
+# class Solution:
+#     def countBits(self, n: int) -> List[int]:
+#         return [self.countOnes(i) for i in range(n+1) ]
+        
+#     def countOnes(self,n):
+#         cnt = 0
+#         while n:
+#             n = n & (n-1)
+#             cnt += 1
+#         return cnt
+        
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        return [self.countOnes(i) for i in range(n+1) ]
-        
-    def countOnes(self,n):
-        cnt = 0
-        while n:
-            n = n & (n-1)
-            cnt += 1
-        return cnt
-        
+        dp = [0] * (n + 1)
+        offset = 1
+
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
