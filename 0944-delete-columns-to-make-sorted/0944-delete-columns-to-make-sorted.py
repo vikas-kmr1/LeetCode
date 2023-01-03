e9bf8bf32abf7991b8ac56693bd00c3c51354c2f
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-01-03 17:01:10 +0530

    Time: 396 ms (41.47%), Space: 14.5 MB (93.05%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,9 +1,5 @@
 class Solution:
 
-#     #O(NlogNM), with N <= 100
-
-#     def minDeletionSize(self, A):
-#         return sum(list(col) != sorted(col) for col in zip(*A))
-    
+#     #O(NM)
       def minDeletionSize(self, A):
         return sum(any(a > b for a, b in zip(col, col[1:])) for col in zip(*A))
