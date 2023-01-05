ec7c414e34155b58d6fc6bf02eda0294dfd269ef
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-01-05 19:56:22 +0530

    Time: 957 ms (24.59%), Space: 16.4 MB (82.39%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,59 +1,59 @@
-class Solution:
-    def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid or not grid[0]:
-            return 0
-
-        islands = 0
-        visit = set()
-        rows, cols = len(grid), len(grid[0])
-
-        def dfs(r, c):
-            if (
-                r not in range(rows)
-                or c not in range(cols)
-                or grid[r][c] == "0"
-                or (r, c) in visit
-            ):
-                return
-
-            visit.add((r, c))
-            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-            for dr, dc in directions:
-                dfs(r + dr, c + dc)
-
-        for r in range(rows):
-            for c in range(cols):
-                if grid[r][c] == "1" and (r, c) not in visit:
-                    islands += 1
-                    dfs(r, c)
-        return islands
-
-
-# def dfs(grid, row ,column):
-#     if row < 0 or column < 0 or row >= len(grid) or column >= len(grid[0]):
-#         return 
-    
-#     if grid[row][column] == '1':
-#         grid[row][column] = '#'
-#         dfs(grid,row+1,column)
-#         dfs(grid,row-1,column)
-#         dfs(grid,row,column+1)
-#         dfs(grid,row,column-1)
-
-
 # class Solution:
 #     def numIslands(self, grid: List[List[str]]) -> int:
+#         if not grid or not grid[0]:
+#             return 0
+
+#         islands = 0
+#         visit = set()
+#         rows, cols = len(grid), len(grid[0])
+
+#         def dfs(r, c):
+#             if (
+#                 r not in range(rows)
+#                 or c not in range(cols)
+#                 or grid[r][c] == "0"
+#                 or (r, c) in visit
+#             ):
+#                 return
+
+#             visit.add((r, c))
+#             directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+#             for dr, dc in directions:
+#                 dfs(r + dr, c + dc)
+
+#         for r in range(rows):
+#             for c in range(cols):
+#                 if grid[r][c] == "1" and (r, c) not in visit:
+#                     islands += 1
+#                     dfs(r, c)
+#         return islands
+
+
+def dfs(grid, row ,column):
+    if row < 0 or column < 0 or row >= len(grid) or column >= len(grid[0]):
+        return 
+    
+    if grid[row][column] == '1':
+        grid[row][column] = '#'
+        dfs(grid,row+1,column)
+        dfs(grid,row-1,column)
+        dfs(grid,row,column+1)
+        dfs(grid,row,column-1)
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         
-#         rows,cols  = len(grid), len(grid[0])
+        rows,cols  = len(grid), len(grid[0])
         
-#         no_of_islands =  0 
+        no_of_islands =  0 
         
-#         for row in range(rows):
-#             for column in range(cols):
-#                 if grid[row][column] == '1':
-#                     dfs(grid,row,column)
-#                     no_of_islands += 1
+        for row in range(rows):
+            for column in range(cols):
+                if grid[row][column] == '1':
+                    dfs(grid,row,column)
+                    no_of_islands += 1
                 
                     
-#         return no_of_islands
+        return no_of_islands
         
