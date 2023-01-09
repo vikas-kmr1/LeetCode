09830af8c114d617643707f47103f432009f9f00
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-01-09 18:43:26 +0530

    Time: 41 ms (83.52%), Space: 14.4 MB (77.00%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        cloneMap =dict()
+        
+        def clone(node):
+            if node in cloneMap:
+                return cloneMap.get(node)
+            
+            cloneNode = Node(node.val)
+            cloneMap[node] = cloneNode
+            
+            for neighbor in node.neighbors:
+                cloneNode.neighbors.append(clone(neighbor))
+            return cloneNode
+        
+        return clone(node) if node else None
+            
+        
