65db4ad99f39102c4a7bf3dc8f9b3cc0ef42737e
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-01-12 19:14:44 +0530

    Time: 3709 ms (55.13%), Space: 202.6 MB (24.36%) - LeetHub

---
+++ 1519-number-of-nodes-in-the-sub-tree-with-the-same-label/1519-number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -0,0 +1,36 @@
+class Solution:
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+
+        
+
+        def dfs(node: int):
+
+            cnt = Counter()
+
+            if node not in seen:
+
+                cnt[labels[node]] += 1 
+
+                seen.add(node)
+
+                for child in g.get(node, []):
+
+                    cnt += dfs(child)
+
+                ans[node] = cnt[labels[node]]
+
+            return cnt
+
+        
+
+        g, ans, seen = defaultdict(list), [0] * n, set()
+
+        for a, b in edges:
+
+            g[a] += [b]
+
+            g[b] += [a]
+
+        dfs(0)
+
+        return ans
