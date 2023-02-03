4cf6957b7f635580fbbefb0b4aa5810c641e5229
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-02-03 13:56:05 +0530

    Time: 672 ms (52.02%), Space: 27.4 MB (25.26%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -1,20 +1,13 @@
 class Solution:
-    #def numOfSubarrays(self, arr: List[int], k: int, t: int) -> int:   
+    def numOfSubarrays(self, arr: List[int], k: int, t: int) -> int:   
     
-#         currentSum = sum(arr[:k-1])
-#         cnt = 0
-#         j = 0
-#         for i,n in enumerate(arr[k-1:]):
-#             if (currentSum + n) // k  >= t:
-#                 cnt += 1
-#             currentSum += n
-#             currentSum -= arr[j]
-#             j += 1 
+        currentSum = sum(arr[:k-1])
+        cnt = 0
+    
+        for i in range(k-1,len(arr)):
+            if (currentSum + arr[i]) // k  >= t:
+                cnt += 1
+            currentSum += arr[i]
+            currentSum -= arr[i-(k-1)]
 
-#         return cnt
-                
-     def numOfSubarrays(self, a: List[int], k: int, threshold: int) -> int:
-        prefixSum = [0]
-        for i in a:
-            prefixSum.append(i + prefixSum[-1])
-        return sum(prefixSum[i + k] - prefixSum[i] >= k * threshold for i in range(len(a) - k + 1))
+        return cnt
