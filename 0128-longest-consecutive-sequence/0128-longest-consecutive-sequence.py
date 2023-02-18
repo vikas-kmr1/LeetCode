b980c42ff368a1ece7d01c8dd1057115a2d5e0bc
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-02-18 15:43:19 +0530

    Time: 1998 ms (39.82%), Space: 29 MB (26.53%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -21,4 +21,44 @@ class Solution:
         
         
         
-     \
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         hashset = set(nums) # add all distinct nums in hashset
+#         longest_Streak = 0  # initial streak = 0
+#         for num in hashset: 
+            
+#             if (num - 1) not in hashset: # if num - 1 not present in hashset, then
+#                 cur_Streak = 1 # i.e num is in hashset
+#                 cur_num = num 
+                
+#                 while (cur_num+1) in hashset: # now search untill current num + 1 is in hashset 
+#                     cur_num += 1 # increment current num by +1
+#                     cur_Streak += 1 # also increment current streak by +1
+                
+                
+#                 longest_Streak = max(longest_Streak, cur_Streak)# now check max b/w last and current streak
+        
+#         return longest_Streak
+                
+                
+                
+            
+            
+            
+            
