915f8bc6e2f23765cd58236dbec723271be0671f
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-02-23 21:34:29 +0530

    Time: 973 ms (86.89%), Space: 39.6 MB (15.56%) - LeetHub

---
+++ 0502-ipo/0502-ipo.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMaximizedCapital(self, k, W, Profits, Capital):
+        heap = []
+        projects = sorted(zip(Profits, Capital), key=lambda l: l[1])
+        i = 0
+        for _ in range(k):
+            while i < len(projects) and projects[i][1] <= W:
+                heapq.heappush(heap, -projects[i][0])
+                i += 1
+            if heap: W -= heapq.heappop(heap)
+        return W
