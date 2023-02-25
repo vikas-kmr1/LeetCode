915f0e417c9acec1e6a6f6840547db9d056db187
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-02-25 10:48:44 +0530

    Time: 1117 ms (50.67%), Space: 25.1 MB (32.77%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,39 @@
+
+class Solution:
+    # sliding window approach
+    def maxProfit(self, prices: List[int]) -> int:
+        max_pro = 0
+        l,r = 0,1
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                max_pro = max(prices[r] - prices[l],max_pro)
+            
+            else:
+                l = r 
+            r += 1 
+        return max_pro
+        
+        
+        
+        # first approach
+#         max_pro =  0
+#         mini = 1000001
+        
+#         for i in range(len(prices)):
+#             if(prices[i] < mini):
+#                 mini = prices[i]
+#             else:
+#                 max_pro = max(max_pro,prices[i] - mini )
+        
+#         return max_pro
+             
+        
+        
+        
+        
+            
+            
+        
+           
+                
+                
