cebc55d8f8e3ece70f3d4cb3b27ee942e222712e
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-03-06 13:29:57 +0530

    Time: 890 ms (100.00%), Space: 53.2 MB (100.00%) - LeetHub

--- 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.py
+++ 2580-count-ways-to-group-overlapping-ranges/2580-count-ways-to-group-overlapping-ranges.py
@@ -1,21 +1,28 @@
 class Solution:
-    def countWays(self, ranges: List[List[int]]) -> int:
-        ranges.sort()
-        print(ranges)
-        Olap = []
-        Olap.append(ranges[0])
-        n = len(ranges)
-        cnt = 0
-        if n == 1:
-            return 2
-        for i in range(1,n):
-            if  ranges[i][0] <= Olap[-1][1]:
-                cnt += 1
-                Olap[-1][0] = min(ranges[i][0],Olap[-1][0] )
-                Olap[-1][1] = max(ranges[i][1],Olap[-1][1] )
+    #def countWays(self, ranges: List[List[int]]) -> int:
+    def countWays(self, A: List[List[int]]) -> int:
+        pre = -1
+        res = 0
+        for a,b in sorted(A):
+            res += pre < a
+            pre = max(pre, b)
+        return pow(2, res, 10**9+7)
+#         ranges.sort()
+#         print(ranges)
+#         Olap = []
+#         Olap.append(ranges[0])
+#         n = len(ranges)
+#         cnt = 0
+#         if n == 1:
+#             return 2
+#         for i in range(1,n):
+#             if  ranges[i][0] <= Olap[-1][1]:
+#                 cnt += 1
+#                 Olap[-1][0] = min(ranges[i][0],Olap[-1][0] )
+#                 Olap[-1][1] = max(ranges[i][1],Olap[-1][1] )
                 
-            else:
-                Olap.append(ranges[i][:]) 
+#             else:
+#                 Olap.append(ranges[i][:]) 
       
-        return 2 if cnt == n-1 else 2**(n-cnt)%(10**9 + 7)
+#         return 2 if cnt == n-1 else 2**(n-cnt)%(10**9 + 7)
             
