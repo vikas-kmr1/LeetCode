180f5b5f838bc0c7293d593b5045335af81e3b0d
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-03-07 18:24:08 +0530

    Time: 1896 ms (84.50%), Space: 28.5 MB (70.25%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.py
@@ -1,15 +1,19 @@
 class Solution:
     def minimumTime(self, time: List[int], totalTrips: int) -> int:
         
+        
+        #T.C = O(N * log(t)) where N is the len of time and t =  min(time) * totalTrips 
+        
         left,right = 1 , min(time) * totalTrips
         minTime = right
     
         while left <= right:
             t = (left + right)//2
+            
             completedTrips = 0
             for n in time:
                 completedTrips += ( t // n )
-         
+                
             if completedTrips >= totalTrips:
                 right = t - 1
                 minTime = min(t,minTime)
