e40007161e0142838b37df1243038d0274026fa9
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-03-31 14:00:47 +0530

    Time: 754 ms (46.19%), Space: 27.7 MB (77.78%) - LeetHub

--- 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.py
+++ 2587-rearrange-array-to-maximize-prefix-score/2587-rearrange-array-to-maximize-prefix-score.py
@@ -1,15 +1,12 @@
 class Solution:
     def maxScore(self, nums: List[int]) -> int:
         nums.sort(reverse = True)
-        for i in range(1,len(nums)):
-            nums[i] += nums[i-1]
-            
-        cnt = 0
-        for n in nums:
-            if n <= 0:
-                return cnt
-            cnt += 1
-            
-        return cnt
+        pSum = 0
+        for i in range(len(nums)):
+            if nums[i] + pSum <= 0:
+                return i 
+            pSum += nums[i]
+        
+        return len(nums)
             
         
