848ef2f0a0d4b0ffc5b3bedee095a5b4ca6ed693
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-03-31 12:29:59 +0530

    Time: 19 ms (99.41%), Space: 13.9 MB (6.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,24 +1,24 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        one  = 1
-        two  = 1
+#         one  = 1
+#         two  = 1
         
-        for i in range(1,n):
-            temp = one
-            one = one + two
-            two = temp
-        return one
+#         for i in range(1,n):
+#             temp = one
+#             one = one + two
+#             two = temp
+#         return one
    
     #memorize 
-#         cache ={}
-#         cache[1] = 1
-#         cache[2] = 2
-#         def climb(m):
-#             if m in cache:
-#                 return cache[m] 
-#             cache[m] = climb(m -1) + climb(m-2)
-#             return cache[m]
-#         return climb(n)
+        cache ={}
+        cache[1] = 1
+        cache[2] = 2
+        def climb(m):
+            if m in cache:
+                return cache[m] 
+            cache[m] = climb(m -1) + climb(m-2)
+            return cache[m]
+        return climb(n)
     
 # recursive (TLE) #T.C: O(2^n)
 
