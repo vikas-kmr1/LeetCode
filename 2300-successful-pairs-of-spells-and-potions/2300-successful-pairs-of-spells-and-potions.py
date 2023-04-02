72ea076267124d194da90fc49a9a582dbd4c3c7f
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-02 12:53:34 +0530

    Time: 1835 ms (59.20%), Space: 39.5 MB (10.20%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -0,0 +1,31 @@
+class Solution:
+    def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
+        pairs = [] 
+        lookUp = {}
+        potions.sort()
+        for n in spells:
+            cnt = 0 
+            if n in lookUp :
+                cnt = lookUp[n]
+            else:
+                cnt = self.binarySearch(potions,n,success) 
+                lookUp[n] = cnt 
+            pairs.append(cnt)
+        return pairs
+        
+    def binarySearch(self,potions,n,success):
+            left = 0
+            right = len(potions) - 1
+
+            while left <= right:
+                mid = left + (right - left)//2 
+                need = n * potions[mid]
+        
+                if need  >= success:
+                    right = mid - 1
+                else:
+                    left  = mid + 1
+                    
+            return len(potions) - left
+                
+                
