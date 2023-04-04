f3573fa86ce1646fd0bd3ba122bc672b2f264bed
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-04 10:18:32 +0530

    Time: 105 ms (87.60%), Space: 14.6 MB (90.90%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,24 +1,24 @@
-# class Solution:
-#     def partitionString(self, s: str) -> int:
-#         hashSet = set()
-#         cnt = 0
-#         for c in s:
-#             if c in hashSet:
-#                 hashSet.clear()
-#                 cnt += 1 
-#             hashSet.add(c) 
-#         if hashSet:
-#             cnt += 1
-#         return cnt
 class Solution:
     def partitionString(self, s: str) -> int:
-        i, ans, flag = 0, 1, 0
-        while i < len(s):
-            val = ord(s[i]) - ord('a')
-            if flag & (1 << val):
-                flag = 0
-                ans += 1
-            flag |= 1 << val
-            i += 1
-        return ans
+        hashSet = set()
+        cnt = 0
+        for c in s:
+            if c in hashSet:
+                hashSet.clear()
+                cnt += 1 
+            hashSet.add(c) 
+        if hashSet:
+            cnt += 1
+        return cnt
+# class Solution:
+#     def partitionString(self, s: str) -> int:
+#         i, ans, flag = 0, 1, 0
+#         while i < len(s):
+#             val = ord(s[i]) - ord('a')
+#             if flag & (1 << val):
+#                 flag = 0
+#                 ans += 1
+#             flag |= 1 << val
+#             i += 1
+#         return ans
                 
