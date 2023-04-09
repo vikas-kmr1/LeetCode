564f3d761c63e872a4ce8cedf1a850f4bf70cca1
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-09 09:47:57 +0530

    Time: 832 ms (46.67%), Space: 26.1 MB (100.00%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.py
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.py
@@ -1,24 +1,18 @@
 class Solution:
     def diagonalPrime(self, nums: List[List[int]]) -> int:
-        ans = 0
-        
-        for i in range(len(nums)):
-                    if (self.isPrime(nums[i][i])):
-                        ans = max(ans, nums[i][i])
-        for i in range(len(nums)-1,-1,-1):
-             if (self.isPrime( nums[len(nums) - i -1][i])):
-                        ans = max(ans, nums[len(nums) - i -1][i])
-        return ans
-    
-    def isPrime(self,n):
-        # Check if the number is less than or equal to 1, return False if it is
-        if n <= 1:
-            return False
-        # Loop through all numbers from 2 to
-        # the square root of n (rounded down to the nearest integer)
-        for i in range(2, int(n**0.5)+1):
-            # If n is divisible by any of these numbers, return False
-            if n % i == 0:
+        def is_prime(num):
+            if num <= 1:
                 return False
-        # If n is not divisible by any of these numbers, return True
-        return True
+            for i in range(2, int(num ** 0.5) + 1):
+                if num % i == 0:
+                    return False
+            return True
+
+        largest_prime = 0
+        n = len(nums)
+        for i in range(n):
+            if is_prime(nums[i][i]):
+                largest_prime = max(largest_prime, nums[i][i])
+            if is_prime(nums[i][n-i-1]):
+                largest_prime = max(largest_prime, nums[i][n-i-1])
+        return largest_prime
