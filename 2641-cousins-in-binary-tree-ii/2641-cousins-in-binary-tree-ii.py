bb30189b552c145ceadb133c7dce0495e1b67d9a
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-15 23:40:53 +0530

    Time: 1303 ms (100.00%), Space: 162.4 MB (100.00%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.py
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.py
@@ -4,24 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from queue import Queue
+from collections import deque
 
 class Solution:
     def replaceValueInTree(self,root):
-        q = Queue()
-        q.put(root)
+        q = deque()
+        q.append(root)
         root.val = 0
-        while not q.empty():
-            n, total = q.qsize(), 0
+        while q:
+            n, total = len(q), 0
             buf = []
             for i in range(n):
-                node = q.get()
+                node = q.popleft()
                 buf.append(node)
                 if node.left:
-                    q.put(node.left)
+                    q.append(node.left)
                     total += node.left.val
                 if node.right:
-                    q.put(node.right)
+                    q.append(node.right)
                     total += node.right.val
             for node in buf:
                 t = total
