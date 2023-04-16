48832ed8427b1e0b67d910f6ead364689596fca2
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-16 12:52:20 +0530

    Time: 528 ms (100.00%), Space: 35.7 MB (100.00%) - LeetHub

--- 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.py
+++ 2640-find-the-score-of-all-prefixes-of-an-array/2640-find-the-score-of-all-prefixes-of-an-array.py
@@ -1,13 +1,25 @@
+#Using min-heap
+# class Solution:
+#     def findPrefixScore(self, nums: List[int]) -> List[int]:
+#         heap = [-nums[0]]
+#         result = []
+#         runningSum = 0
+#         for num in nums:
+#             maxSum = max(num, -1 * heapq.heappop(heap))
+#             result.append(runningSum + num + maxSum)
+#             runningSum += maxSum+num
+#             heappush(heap,- maxSum)
+#         return result
+        
 class Solution:
     def findPrefixScore(self, nums: List[int]) -> List[int]:
-        heap = [-nums[0]]
+        prevMax = 0 
         result = []
         runningSum = 0
         for num in nums:
-            maxSum = max(num, -1 * heapq.heappop(heap))
-            result.append(runningSum + num + maxSum)
-            runningSum += maxSum+num
-            heappush(heap,- maxSum)
+            prevMax = max(num,prevMax)
+            result.append(runningSum + num + prevMax)
+            runningSum += prevMax + num
         return result
         
         
