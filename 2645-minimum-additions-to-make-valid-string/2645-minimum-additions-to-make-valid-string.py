390d9de947b714c7ce792c15c15434e12bc5305e
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-16 12:40:41 +0530

    Time: 32 ms (92.31%), Space: 13.8 MB (100.00%) - LeetHub

--- 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.py
+++ 2645-minimum-additions-to-make-valid-string/2645-minimum-additions-to-make-valid-string.py
@@ -1,16 +1,23 @@
+# class Solution:
+#     def addMinimum(self, word: str) -> int:
+#         it = 0
+#         result = 0
+#         while it < len(word) :
+#             if word[it:it+3] == "abc":
+#                 it += 3  
+#             elif word[it:it+2] in ["ab","ac","bc"]:
+#                 result += 1
+#                 it += 2
+#             else:
+#                 result += 2
+#                 it += 1
+#         return result
+                    
 class Solution:
     def addMinimum(self, word: str) -> int:
-        it = 0
-        result = 0
-        while it < len(word) :
-            if word[it:it+3] == "abc":
-                it += 3  
-            elif word[it:it+2] in ["ab","ac","bc"]:
-                result += 1
-                it += 2
-            else:
-                result += 2
-                it += 1
-        return result
-                    
-                    
+        times = 0
+        prev = 'z'
+        for c in word:
+            times += c <= prev
+            prev = c
+        return times * 3 - len(word)
