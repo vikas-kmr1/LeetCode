469512dafdb158576fbef3c4220584aa306803b1
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-22 20:48:02 +0530

    Time: 1515 ms (19.62%), Space: 13.9 MB (9.19%) - LeetHub

---
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -0,0 +1,13 @@
+class Solution:
+    def countTriples(self, n: int) -> int:
+        freq = { i**2 :i for i in range(1,n+1)}
+        cnt= 0
+        for a in range(1,n+1):
+            b = 1
+            while a**2 + b**2 <= n**2:
+                if (a**2 + b**2) in freq:
+                    cnt += 1
+                b += 1
+        return cnt
+                
+        
