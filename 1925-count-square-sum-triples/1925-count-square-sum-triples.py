982bd32b4bf2a6b58c5d50d3b3e7283109377768
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-22 20:49:09 +0530

    Time: 822 ms (31.31%), Space: 14 MB (9.19%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.py
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.py
@@ -3,11 +3,9 @@ class Solution:
         freq = { i**2 :i for i in range(1,n+1)}
         cnt= 0
         for a in range(1,n+1):
-            b = 1
-            while a**2 + b**2 <= n**2 and b <= n:
+            for b in range(1,n+1):
                 if (a**2 + b**2) in freq:
                     cnt += 1
-                b += 1
         return cnt
                 
         
