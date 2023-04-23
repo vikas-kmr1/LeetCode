fc0ead28066f8a59d3ed139b76ff3926dc573eba
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-23 12:40:01 +0530

    Time: 6515 ms (25.00%), Space: 29.2 MB (12.50%) - LeetHub

--- 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.py
+++ 2653-sliding-subarray-beauty/2653-sliding-subarray-beauty.py
@@ -1,14 +1,11 @@
+from sortedcontainers import SortedList
+
 class Solution:
     def getSubarrayBeauty(self, nums: List[int], k: int, x: int) -> List[int]:
-        counter, ans = [0] * 50, [0] * (len(nums) - k + 1)
-        for i in range(len(nums)):
-            if nums[i] < 0: counter[nums[i] + 50] += 1
-            if i - k >= 0 and nums[i - k] < 0: counter[nums[i - k] + 50] -= 1
-            if i - k + 1 < 0: continue
-            count = 0
-            for j in range(50):
-                count += counter[j]
-                if count >= x:
-                    ans[i - k + 1] = j - 50
-                    break
-        return ans
+        ans = []
+        vals = SortedList()
+        for i, v in enumerate(nums): 
+            vals.add(v)
+            if i >= k: vals.remove(nums[i-k])
+            if i >= k-1: ans.append(min(0, vals[x-1]))
+        return ans 
