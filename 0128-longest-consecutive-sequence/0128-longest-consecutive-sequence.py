289ac489b46eb3f96523ecea928e6d29cac56798
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-26 18:33:53 +0530

    Time: 2072 ms (15.95%), Space: 31.2 MB (14.86%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -2,61 +2,15 @@
 #S.C: O(n)
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        numSet = set(nums)
+        hashset = set(nums)
         longest = 0
-
         for n in nums:
-            # check if its the start of a sequence
-            if (n - 1) not in numSet:
-                length = 1
-                while (n + length) in numSet:
-                    length += 1
-                longest = max(length, longest)
+            if n - 1  not in hashset:
+                streak = 1
+                while n + streak in hashset:
+                    streak += 1
+                longest = max(longest,streak)
         return longest
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         hashset = set(nums) # add all distinct nums in hashset
-#         longest_Streak = 0  # initial streak = 0
-#         for num in hashset: 
-            
-#             if (num - 1) not in hashset: # if num - 1 not present in hashset, then
-#                 cur_Streak = 1 # i.e num is in hashset
-#                 cur_num = num 
-                
-#                 while (cur_num+1) in hashset: # now search untill current num + 1 is in hashset 
-#                     cur_num += 1 # increment current num by +1
-#                     cur_Streak += 1 # also increment current streak by +1
-                
-                
-#                 longest_Streak = max(longest_Streak, cur_Streak)# now check max b/w last and current streak
-        
-#         return longest_Streak
-                
-                
                 
             
             
