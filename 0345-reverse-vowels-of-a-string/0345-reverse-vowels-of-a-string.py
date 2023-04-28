cc4ba0337e3ecae82061619e6b54eb8e97ceb597
Vikas Kumar <VICKYRULES1705@GMAIL.COM>
2023-04-28 20:48:22 +0530

    Time: 71 ms (30.22%), Space: 17.5 MB (5.83%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,29 +1,22 @@
 class Solution:
-    def reverseVowels(self, s):
-        VOWELS: set = { "a", "e", "i", "o", "u", "A", "E", "I", "O", "U" }
-            
-        characters: list[str] = list(s)
-        left, right = 0, len(characters) - 1
-        while left < right:
-            left_value, right_value = characters[left], characters[right]
-            if left_value in VOWELS and right_value in VOWELS:
-                characters[left], characters[right] = characters[right], characters[left]
-                left += 1
-                right -= 1
-            elif left_value in VOWELS and right_value not in VOWELS:
-                right -= 1
-            elif left_value not in VOWELS and right_value in VOWELS:
-                left += 1
-            else:
-                left += 1
-                right -= 1
+    def reverseVowels(self, s: str) -> str:
+        s = [*s]
+        i , j = 0 , len(s) - 1
         
-        return "".join(characters)
-
-
-
-
-
-
-
+        vowels = ('a','e','i','o','u')
+        while i < j:
         
+            while j > i and s[j].lower() not in vowels:
+                j -= 1
+                
+            while i < j and  s[i].lower() not in vowels:
+                i += 1
+                
+            if s[i].lower() in vowels and s[j].lower() in vowels:
+                s[i],s[j] = s[j], s[i]
+                i += 1
+                j -= 1
+        
+        
+        return "".join(s)
+                
