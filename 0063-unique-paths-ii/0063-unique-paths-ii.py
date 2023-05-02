fe0ab2b04a698bc57aa6b5991516c4c5ddd6b79d
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-05-02 21:59:53 +0530

    Time: 67 ms (5.45%), Space: 16.7 MB (5.99%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,28 +1,28 @@
 class Solution:
     def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
-        M, N = len(grid), len(grid[0])
-        dp = [0] * N
-        dp[N-1] = 1
-
-        # Time: O(N*M), Space: O(N)
-        for r in range(M-1,-1,-1):
-            for c in range(N-1,-1,-1):
-                if grid[r][c]:
-                    dp[c] = 0
-                elif c + 1 < N:
-                    dp[c] = dp[c] + dp[c + 1]
-        return dp[0]
-
-
-#         # Time: O(N*M), Space: O(N*M)
 #         M, N = len(grid), len(grid[0])
-#         dp = {(M - 1, N - 1): 1}
+#         dp = [0] * N
+#         dp[N-1] = 1
 
-#         def dfs(r, c):
-#             if r == M or c == N or grid[r][c]:
-#                 return 0
-#             if (r, c) in dp:
-#                 return dp[(r, c)]
-#             dp[(r, c)] = dfs(r + 1, c) + dfs(r, c + 1)
-#             return dp[(r, c)]
-#         return dfs(0, 0)
+#         # Time: O(N*M), Space: O(N)
+#         for r in range(M-1,-1,-1):
+#             for c in range(N-1,-1,-1):
+#                 if grid[r][c]:
+#                     dp[c] = 0
+#                 elif c + 1 < N:
+#                     dp[c] = dp[c] + dp[c + 1]
+#         return dp[0]
+
+
+        # Time: O(N*M), Space: O(N*M)
+        M, N = len(grid), len(grid[0])
+        dp = {(M - 1, N - 1): 1}
+
+        def dfs(r, c):
+            if r == M or c == N or grid[r][c]:
+                return 0
+            if (r, c) in dp:
+                return dp[(r, c)]
+            dp[(r, c)] = dfs(r + 1, c) + dfs(r, c + 1)
+            return dp[(r, c)]
+        return dfs(0, 0)
