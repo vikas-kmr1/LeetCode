315f59c0c4fabff081253718a4b99ff2918493a3
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-06-11 11:25:16 +0530

    Time: 658 ms (76.16%), Space: 45.5 MB (47.55%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -0,0 +1,22 @@
+class SnapshotArray(object):
+
+    def __init__(self, n):
+        self.A = [[[-1, 0]] for _ in range(n)]
+        self.snap_id = 0
+
+    def set(self, index, val):
+        self.A[index].append([self.snap_id, val])
+
+    def snap(self):
+        self.snap_id += 1
+        return self.snap_id - 1
+
+    def get(self, index, snap_id):
+        i = bisect.bisect(self.A[index], [snap_id + 1]) - 1
+        return self.A[index][i][1]
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
