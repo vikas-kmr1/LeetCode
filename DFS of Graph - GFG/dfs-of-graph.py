a18e2e9acf27472b1576f15c9db764f741244ed3
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-08-01 16:57:49 +0530

    Added solution - LeetHub

---
+++ DFS of Graph - GFG/dfs-of-graph.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class Solution:
+    
+    #Function to return a list containing the DFS traversal of the graph.
+    def dfsOfGraph(self, V, adj):
+        # code here
+    
+        visited = set()
+        res = []
+        
+        
+        
+        def helper(node):
+            
+            res.append(node)
+            visited.add(node)
+            
+            for v in adj[node]:
+                if v not in visited:
+                    helper(v)
+        helper(0)
+        return res
+        
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__ == '__main__':
+    T=int(input())
+    while T>0:
+        V,E=map(int,input().split())
+        adj=[[] for i in range(V+1)]
+        for i in range(E):
+            u,v=map(int,input().split())
+            adj[u].append(v)
+            adj[v].append(u)
+        ob=Solution()
+        ans=ob.dfsOfGraph(V,adj)
+        for i in range(len(ans)):
+            print(ans[i],end=" ")
+        print()
+        T-=1
+# } Driver Code Ends
