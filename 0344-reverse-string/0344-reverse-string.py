42ae74dcfa0df34a2ed6eb6b975dd84883da3fa1
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-08-10 17:39:17 +0530

    Time: 413 ms (5.22%), Space: 70.8 MB (5.01%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.py
@@ -0,0 +1,33 @@
+# def reverseString(i:int ,s:List[str]):
+#     if i >= len(s)//2:
+#         return
+#     else:
+#         s[i],s[len(s)-i-1] = s[len(s)-i-1], s[i]
+#         reverseString(i+1,s)
+        
+
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         reverseString(0,s)
+        
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+#         ptr1 = 0
+#         ptr2 = len(s)-1
+        
+#         while ptr1 < ptr2:
+#             s[ptr1],s[ptr2]  = s[ptr2],s[ptr1]
+#             ptr1+=1
+#             ptr2-=1
+        
+        def helper(s):
+            if len(s) == 1:
+                return s
+            
+            item = s.pop(0)
+            recievedS = helper(s)
+            recievedS.append(item)
+            return recievedS
+        return helper(s)
+           
+        
