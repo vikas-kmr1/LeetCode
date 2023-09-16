db577dc81fda09154ef5a0fcee6e3f9b94fba7bb
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-09-16 08:02:13 +0530

    Time: 422 ms (80.48%), Space: 17.8 MB (71.44%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,35 +1,55 @@
+
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        # Calculate the total sum of the numbers in the array
-        total_sum = sum(nums)
-        
-        # Check if the total sum is odd, in which case it's impossible to partition
-        if total_sum % 2:
+        if sum(nums) % 2:
             return False
+
+        dp = set()
+        dp.add(0)
+        target = sum(nums) // 2
+
+        for i in range(len(nums) - 1, -1, -1):
+            nextDP = set()
+            for t in dp:
+                if (t + nums[i]) == target:
+                    return True
+                nextDP.add(t + nums[i])
+                nextDP.add(t)
+            dp = nextDP
+        return False
+
+# class Solution:
+#     def canPartition(self, nums: List[int]) -> bool:
+#         # Calculate the total sum of the numbers in the array
+#         total_sum = sum(nums)
         
-        # Calculate the target sum for each partition
-        target_sum = total_sum // 2
+#         # Check if the total sum is odd, in which case it's impossible to partition
+#         if total_sum % 2:
+#             return False
         
-        # Initialize a dynamic programming table to track if a sum is achievable
-        # dp[i] will be True if a sum of i is achievable, otherwise False
-        dp = [1 if i == 0 else 0 for i in range(target_sum + 1)]
+#         # Calculate the target sum for each partition
+#         target_sum = total_sum // 2
         
-        for num in nums:
-            # Create a set to track the achieved sums for the current number
-            achieved_sums = set()
+#         # Initialize a dynamic programming table to track if a sum is achievable
+#         # dp[i] will be True if a sum of i is achievable, otherwise False
+#         dp = [1 if i == 0 else 0 for i in range(target_sum + 1)]
+        
+#         for num in nums:
+#             # Create a set to track the achieved sums for the current number
+#             achieved_sums = set()
             
-            # Iterate through the dp table from 'num' to 'target_sum'
-            for j in range(num, target_sum + 1):
-                # Check if we can achieve sum 'j' by including or excluding the current number 'num'
-                if dp[j] == 0 and dp[j - num] and (j - num) not in achieved_sums:
-                    dp[j] = 1
-                    achieved_sums.add(j)
-                # If the target sum is achieved, return True
-                if dp[target_sum]:
-                    return 1
+#             # Iterate through the dp table from 'num' to 'target_sum'
+#             for j in range(num, target_sum + 1):
+#                 # Check if we can achieve sum 'j' by including or excluding the current number 'num'
+#                 if dp[j] == 0 and dp[j - num] and (j - num) not in achieved_sums:
+#                     dp[j] = 1
+#                     achieved_sums.add(j)
+#                 # If the target sum is achieved, return True
+#                 if dp[target_sum]:
+#                     return 1
         
-        # If no partition is found, return False
-        return 0
+#         # If no partition is found, return False
+#         return 0
         
         
         # O(2^len(nums))        
