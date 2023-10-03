61de657052c8163b1e2638a111b8d86aa788b9f2
νιкαѕ кυмαя <VICKYRULES1705@GMAIL.COM>
2023-10-03 19:48:08 +0530

    Added solution - LeetHub

---
+++ Bipartite Graph - GFG/bipartite-graph.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+class Solution:
+    def isBipartite(self, V,graph):
+        color = {}
+        def dfs(pos):
+            for i in graph[pos]:
+                if i in color:
+                    if color[i] == color[pos]:
+                        return False
+                else:
+                    color[i] = 1 - color[pos]
+                    if not dfs(i):
+                        return False
+            return True
+        for i in range(len(graph)):
+            if i not in color:
+                color[i] = 0
+                if not dfs(i):
+                    return False
+        return True
+		#code here
+
+
+#{ 
+ # Driver Code Starts
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		V, E = map(int, input().strip().split())
+		adj = [[] for i in range(V)]
+		for i in range(E):
+			u, v = map(int, input().strip().split())
+			adj[u].append(v)
+			adj[v].append(u)
+		obj = Solution()
+		ans = obj.isBipartite(V, adj)
+		if(ans):
+			print("1")
+		else:
+			print("0")
+
+# } Driver Code Ends
